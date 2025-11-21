988925ae15a2acce3e196bbe6bb645acf5e9e213
W.G.I.Adeesha <induruadeesha2356@gmail.com>
2025-11-21 23:37:34 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config();// Load environment variables from .env file
 
 console.log('Hello World');
 
@@ -22,9 +23,15 @@ app.get('/',(req,res)=>{
 app.use('/public', express.static(__dirname + '/public'));// Serve static assets
 
 app.get('/json', (req, res) => {// Serve a JSON response
-    res.json({"message": "Hello json"});// Send a JSON response
+    //res.json({"message": "Hello json"});// Send a JSON response
+    var message = "Hello json";
+    //check environment variable === "uppercase" at .env file
+    if(process.env.MESSAGE_STYLE === "uppercase"){// Check environment variable
+        message = message.toUpperCase();// Convert message to uppercase
+    }
+    res.json({"message": message});// Send a JSON response
 });
-
+ 
 
 
 
